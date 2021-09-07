83a5d50c5dd302a2a33ca543de89716de2a2f9cd
Shema Christophe <jeanchristopheshema@gmail.com>
2021-09-06 18:20:50 -0700

    adding file/folder

---
+++ custom-scripts/ansible-ubuntu-config.sh
@@ -0,0 +1,12 @@
+sudo apt update
+
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt -y install python3-pip
+
+#install ansible
+sudo apt -y install ansible
+
+#install azure required modules 
+sudo apt -y install ansible[azure]
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+sudo pip3 install -r requirements-azure.txt 
